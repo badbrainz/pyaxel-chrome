8009cfdbf23156000750bf7e91649bf31e7ede25
wormboy <wormboy@localhost>
2015-03-21 17:33:53 -0700

    remove help section links

--- views/help.js
+++ views/help.js
@@ -1,15 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    $('report').onclick = function() {
-        chrome.tabs.create({
-            url: 'https://chrome.google.com/webstore/support/' + chrome.runtime.id + '#bug'
-        });
-    };
-
-    $('manual').addEventListener('click', function() {
-        chrome.tabs.create({
-            'url': 'https://code.google.com/p/pyaxelws/wiki/Extension'
-        });
-    });
-
-    $('product-version').innerText = chrome.runtime.getManifest().version;
+    var manifest = chrome.runtime.getManifest();
+    $('product-description').textContent = manifest.description;
+    $('product-version').innerText = manifest.version;
 });
